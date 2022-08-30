494e12b386f78b795d8e8434bbffddd9830af0fc
deepakbharti755 <deepakbharti755@gmail.com>
2022-08-30 21:04:14 +0530

    make it better

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from 'react'
+import ShowTodo from './ShowTodo'
+import './Todo.css'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [task, setTask] = useState("")
+    const [data, setData] = useState([])
+
+    const onChangeHandler = (e) => {
+        setTask(e.target.value)
+    }
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+        const newData = task;
+        setData([...data, newData])
+
+        setTask('')
+    }
+
+    const deleteItem =(a)=>{
+        const finalData = data.filter((curEle,index)=>{
+            return index !== a;
+        })
+        setData(finalData)
+    }
+
+    return (
+        <div className="container">
+            <div className="row justify-content-center align-items-center main-row">
+                <div className="col shadow main-col bg-white">
+                    <div className="row bg-primary text-white">
+                        <div className="col  p-2">
+                            <h4 className='text-center'><h1>ToDo List</h1></h4>
+                        </div>
+                    </div>
+                    <form onSubmit={submitHandler}>
+                        <div className="row justify-content-between text-white p-2">
+                            <div className="form-group flex-fill mb-2 col-9">
+                                <input id="todo-input" type="text" className="form-control" value={task} onChange={onChangeHandler} />
+                                <button type="submit" className="btn btn-primary mb-2 ml-2 col-3">Add task</button>
+                            </div>
+
+                            
+                        </div>
+                    </form>
+
+                    {data.map((value, index) => {
+                        return <ShowTodo
+                            key={index}
+                            id={index}
+                            task={value}
+                            onSelcet={deleteItem}
+                        />
+                    })}
+
+
+                </div>
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
